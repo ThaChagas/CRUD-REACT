5f15c0eee655e390d655f00a3f2cac3971c24c43
Thaís Chagas <tha.neivas@gmail.com>
2024-04-05 18:00:23 -0300

    crud react commit

---
+++ src/pages/home/index.js
@@ -0,0 +1,12 @@
+import image from "./images/foto.jpeg"
+import "./index.css"
+
+export default function Produtos(){
+    return (
+
+        <div className="foto">
+            <img src={image} alt="foto" />
+        </div>
+        
+    )
+}
